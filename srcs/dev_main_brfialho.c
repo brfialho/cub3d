ae3ea983280b51691ed2e5c7b187665a7a4c0a2e
brfialho <brunolegal3@gmail.com>
2026-03-21 00:13:28 -0300

    wip: load file logic

--- srcs/dev_main_brfialho.c
+++ srcs/dev_main_brfialho.c
@@ -6,15 +6,40 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 17:31:11 by brfialho          #+#    #+#             */
-/*   Updated: 2026/03/19 17:35:48 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/03/21 00:04:08 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+#include "main.h"
+
+void	parsing(t_tab *map, char **path, int *colors, char *file)
+{
+	(void)map;
+	(void)path;
+	(void)colors;
+	int	fd;
+	char *s;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		exit(1);
+	while ((s = get_next_line(fd)))
+		ft_printf("%d : %s", ft_strlen(s), s);
+	
+	close(fd);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	ft_printf("Bruno\n");
+	t_game	game;
+
+	if (argc != 2)
+	{
+		ft_printf("Wrong number of arguments\n");
+		exit(EXIT_FAILURE);
+	}
+	ft_bzero(&game, sizeof(t_game));
+	parsing(&game.map, game.path, game.mlx.colors, argv[1]);
 }
