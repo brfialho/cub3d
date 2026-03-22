d3f1085e3a4463bc1730c14b16557940024150fd
brfialho <brunolegal3@gmail.com>
2026-03-22 04:30:28 -0300

    feat: parser

--- srcs/dev_main_brfialho.c
+++ srcs/dev_main_brfialho.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 17:31:11 by brfialho          #+#    #+#             */
-/*   Updated: 2026/03/22 03:20:28 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/03/22 04:29:33 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,11 +63,47 @@ t_bool	check_for_missing_elements(t_game *game)
 	return (SUCCESS);
 }
 
+void	set_map(t_tab *map, char *dump_map, size_t max_len)
+{
+	char	**split;
+	int		i;
+	size_t	split_len;
+
+	split = ft_split(dump_map, '\n');
+	split_len = ft_split_len(split);
+	ft_tab_init_alloc(map, split_len + 1, max_len + 1, sizeof(char));
+	i = -1;
+	while (split[++i])
+		ft_memcpy(map->tab[i], split[i], ft_strlen(split[i]));
+	ft_split_free(split);
+}
+
 t_bool	parse_map(t_game *game, t_parser *parser)
 {
-	(void)parser;
+	char	*dump_map;
+	char	*buffer;
+	size_t	max_len;
+	size_t	buffer_len;
+
 	if (check_for_missing_elements(game))
 		return (FAILURE);
+	(void)parser;
+	dump_map = ft_strdup("");
+	buffer = ft_strdup(parser->line);
+	max_len = 0;
+	while (buffer)
+	{
+		buffer_len = ft_strlen(buffer);
+		if (max_len < buffer_len)
+			max_len = ft_strlen(buffer);
+		if (!ft_strcmp(buffer, "\n")
+			|| !ft_str_allinset(buffer, VALID_MAP_CHARS))
+			return (free(buffer), free(dump_map), FAILURE);
+		dump_map = ft_strjoin_free(dump_map, buffer, TRUE, TRUE);
+		buffer = get_next_line(parser->fd);
+	}
+	set_map(&game->map, dump_map, max_len);
+	free(dump_map);
 	return (SUCCESS);
 }
 
@@ -164,6 +200,12 @@ int	main(int argc, char **argv)
 		}
 	ft_printf("HEX: %X\n", game.mlx.colors[CEILING]);
 	ft_printf("HEX: %X\n", game.mlx.colors[FLOOR]);
+	if (game.map.tab)
+	{
+		ft_printf("\nROWS: %d COLS: %d\n", game.map.rows, game.map.cols);
+		ft_split_print((char **)game.map.tab);
+		ft_tab_free_content(&game.map);
+	}
 }
 // int main()
 // {
