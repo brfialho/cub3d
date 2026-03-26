249b0418a4bf7b49fdd7f33ac6c86b3e29936282
brfialho <brunolegal3@gmail.com>
2026-03-26 00:09:56 -0300

    chore:?? player carangueijo movement

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 16:26:21 by brfialho          #+#    #+#             */
-/*   Updated: 2026/03/25 23:36:14 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/03/26 00:09:12 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	move_player(t_game *game)
 	// double	DirX;
 	// double	DirY;
 
-	game->player[DIR_X] = 1;
+	game->player[DIR_X] = -1;
 	game->player[DIR_Y] = 0;
 
 	// game->player[DIR_X] += game->key_is_pressed[65361] * (TURN_SPEED * ((DirY < 0) - (DirY >= 0)));
@@ -55,6 +55,14 @@ void	move_player(t_game *game)
 								+ ((game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_Y] * -1)
 								+ ((game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_Y] * -1)
 							))
+							+ ((game->key_is_pressed['a']) * (
+								((game->player[DIR_X] > 0) * (game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_Y])
+								+ ((game->player[DIR_X] > 0) * (game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_Y])
+								+ ((game->player[DIR_X] < 0) * (game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_Y])
+								+ ((game->player[DIR_X] < 0) * (game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_Y])
+								+ ((game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_Y])
+								+ ((game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_Y])
+							))
 							);
 	game->player[POS_Y] += ((game->key_is_pressed['w'] * (MOVE_SPEED * game->player[DIR_Y]))
 							+ (game->key_is_pressed['s'] * -1 * (MOVE_SPEED * game->player[DIR_Y]))
@@ -65,6 +73,14 @@ void	move_player(t_game *game)
 								+ ((game->player[DIR_X] < 0) * (game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_X])
 								+ ((game->player[DIR_X] < 0) * MOVE_SPEED * game->player[DIR_X])
 								+ ((game->player[DIR_X] > 0) * MOVE_SPEED * game->player[DIR_X])
+							))
+							+ ((game->key_is_pressed['a']) * (
+								((game->player[DIR_X] > 0) * (game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_X] * -1)
+								+ ((game->player[DIR_X] > 0) * (game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_X] * -1)
+								+ ((game->player[DIR_X] < 0) * (game->player[DIR_Y] > 0) * MOVE_SPEED * game->player[DIR_X] * -1)
+								+ ((game->player[DIR_X] < 0) * (game->player[DIR_Y] < 0) * MOVE_SPEED * game->player[DIR_X] * -1)
+								+ ((game->player[DIR_X] < 0) * MOVE_SPEED * game->player[DIR_X] * -1)
+								+ ((game->player[DIR_X] > 0) * MOVE_SPEED * game->player[DIR_X] * -1)
 							))
 							);
 }
