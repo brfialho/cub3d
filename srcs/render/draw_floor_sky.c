0eaae5d83ed8bce3e19209048fef47fa9ad0f7ee
brfialho <brunolegal3@gmail.com>
2026-04-06 16:44:58 -0300

    fix: not using rgb

--- srcs/render/draw_floor_sky.c
+++ srcs/render/draw_floor_sky.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 22:25:32 by gbercaco          #+#    #+#             */
-/*   Updated: 2026/04/06 13:10:16 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/04/06 16:41:44 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	draw_floor_and_sky(t_mlx *mlx)
 	{
 		x = -1;
 		while (++x < WIN_WIDTH)
-			put_pixel(mlx, x, y, 0xADD8E6);
+			put_pixel(mlx, x, y, mlx->colors[CEILING]);
 	}
 	while (y < WIN_HEIGHT)
 	{
 		x = -1;
 		while (++x < WIN_WIDTH)
-			put_pixel(mlx, x, y, 0x8B4513);
+			put_pixel(mlx, x, y, mlx->colors[FLOOR]);
 		y++;
 	}
 }
